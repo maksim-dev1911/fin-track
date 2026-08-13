cc421a97011891143aaf7b67126dda1f8f62a1e5
Maks <mshvetsov68@gmail.com>
2026-08-13 21:07:41 +0300

    feat: fix

--- src/shared/lib/format-money.ts
+++ src/shared/lib/format-money.ts
@@ -7,8 +7,11 @@ export const formatTransactionAmount = (
 ) => {
   const amount = Math.abs(cents) / 100;
 
-  return `${type === 'income' ? '+' : '-'}$${amount.toLocaleString('en-US', {
-    minimumFractionDigits: options?.hideDecimals ? 0 : 2,
-    maximumFractionDigits: options?.hideDecimals ? 0 : 2,
-  })}`;
+  return `${type === 'income' ? '+' : type === 'expense' ? '-' : ''}$${amount.toLocaleString(
+    'en-US',
+    {
+      minimumFractionDigits: options?.hideDecimals ? 0 : 2,
+      maximumFractionDigits: options?.hideDecimals ? 0 : 2,
+    },
+  )}`;
 };

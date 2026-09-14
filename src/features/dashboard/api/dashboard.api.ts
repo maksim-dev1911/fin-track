915717c201cec312d35b5542f4692c51400d1a5f
Maks <mshvetsov68@gmail.com>
2026-09-14 14:56:06 +0300

    feat: fixed comments

--- src/features/dashboard/api/dashboard.api.ts
+++ src/features/dashboard/api/dashboard.api.ts
@@ -1,5 +1,7 @@
+import type { AnalyticResponse } from '@/features/dashboard/types/analytics.types.ts';
 import { apiClient } from '@/shared/api/client';
 import { endpoints } from '@/shared/api/endpoints.ts';
+import type { ApiResponse } from '@/shared/api/types.ts';
 
 class DashboardApi {
   private readonly client: typeof apiClient;
@@ -9,7 +11,7 @@ class DashboardApi {
   }
 
   async getAnalytics() {
-    const response = await this.client.get(endpoints.ANALYTICS);
+    const response = await this.client.get<ApiResponse<AnalyticResponse[]>>(endpoints.ANALYTICS);
 
     return response.data.data;
   }
